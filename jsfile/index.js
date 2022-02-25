526d740850d4d83817a49d715927c2c26923c99e
Gajraj Singh Shekhawat <gaju2gajraj@gmail.com>
2022-02-25 18:37:06 +0530

    login part Done

--- jsfile/index.js
+++ jsfile/index.js
@@ -44,7 +44,7 @@ function showlogin() {
     document.querySelector("#loginpage").style.display = "block";
     // document.querySelector('body').style.opacity = "0.3";
     document.querySelector("#loginpage").style.opacity = "1";
-  
+
 }
 document.querySelector("#closebtn").addEventListener("click", hidelogin)
 
@@ -52,4 +52,48 @@ function hidelogin() {
     document.querySelector("#loginpage").style.display = "none";
     document.querySelector('body').style.opacity = "1";
 
+}
+
+document.querySelector("#loginform").addEventListener("submit", function () {
+    event.preventDefault()
+    var phnnumber = loginform.loginphone.value;
+    // console.log('number:', typeof(phnnumber))
+    if (phnnumber.length == 10) {
+        document.querySelector("#loginbottom").innerHTML = "";
+        var para = document.createElement("p")
+        para.innerText = "Enter OTP sent to +91 " + phnnumber;
+        var otpinput = document.createElement("input");
+        otpinput.setAttribute("type", "number");
+        otpinput.setAttribute('placeholder', "Enter OTP here")
+        var btn = document.createElement("input");
+        btn.setAttribute('type', 'submit');
+        btn.setAttribute('value', "CONTINUE");
+        btn.style.backgroundColor = "#10847e";
+        btn.style.color = "white"
+        var paralast = document.createElement('p');
+        var error = document.createElement('p');
+        error.style.color="red"
+
+        paralast.innerText = "By clicking continue, you agree with our Privacy Policy"
+        btn.addEventListener('click', function () {
+            otpconfirm(otpinput.value,error)
+        })
+
+        document.querySelector("#loginbottom").append(para, otpinput,error, btn, paralast);
+
+    } else {
+        document.querySelector("#errormsg").innerText = "Enter a valid mobile number"
+        document.querySelector("#errormsg").style.color = "red";
+    }
+
+
+})
+function otpconfirm(v, error) {
+    // console.log(v)
+    if (v == "1234") {
+        document.querySelector("#loginpage").style.display = "none";
+        document.querySelector("#loginbtn").innerText="Gajraj"
+    } else {
+        error.innerText ="The code entered is incorrect."
+    }
 }
